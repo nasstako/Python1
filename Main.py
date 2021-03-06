3bebafb9d075e382092c9f2c3180b6212aabca96
nasstako <80150148+nasstako@users.noreply.github.com>
2021-03-06 04:45:28 -0500

    tutorial 14

--- Main.py
+++ Main.py
@@ -1,56 +1,75 @@
-# episode operator bitwise, operasi biner, binary
+# operasi yang dapat dilakukan dengan penyingkatan
+# operasi ditambah dengan assignment
 
-a = 9
-b = 5
+a = 5 # adalah assignment
+print("nilai a =",a)
 
-# bitwise OR (|)
-c = a | b
-print('\n=========OR=========')
-print('nilai :',a,' , binary :',format(a,'08b'))
-print('nilai :',b,' , binary :',format(b,'08b'))
-print('----------------------------- (|)')
-print('nilai :',c,' , binary :',format(c,'08b'))
+a += 1 # artinya adalah a = a + 1
+print("nilai a += 1, nilai a menjadi",a)
 
-# bitwise AND (&)
-c = a & b
-print('\n=========AND========')
-print('nilai :',a,' , binary :',format(a,'08b'))
-print('nilai :',b,' , binary :',format(b,'08b'))
-print('----------------------------- (&)')
-print('nilai :',c,' , binary :',format(c,'08b'))
+a -= 2 # artinya adalah a = a - 2
+print("nilai a -= 2, nilai a menjadi",a)
 
-# bitwise XOR (^)
-c = a ^ b
-print('\n=========XOR========')
-print('nilai :',a,' , binary :',format(a,'08b'))
-print('nilai :',b,' , binary :',format(b,'08b'))
-print('----------------------------- (^)')
-print('nilai :',c,' , binary :',format(c,'08b'))
+a *= 5 # artinya adalah a = a * 5
+print("nilai a *= 5, nilai a menjadi",a)
 
-# bitwise NOT (~)
-c = ~a
-print('\n=========NOT========')
-print('nilai :',a,' , binary :',format(a,'08b'))
-print('----------------------------- (~)')
-print('nilai :',c,' , binary :',format(c,'08b'))
-print('----------------------------- (^)')
-d = 0b0000001001
-e = 0b1111111111
-print('nilai :',e^d,' , binary :',format(e^d,'08b'))
+a /= 2 # artinya adalah a = a / 2
+print("nilai a /= 2, nilai a menjadi",a)
 
-# shifting
+b = 10
+print("\nnilai b =",b)
 
-# shift right (>>)
-c = a >> 2
-print('\n=========>>=========')
-print('nilai :',a,' , binary :',format(a,'08b'))
-print('----------------------------- (>>)')
-print('nilai :',c,' , binary :',format(c,'08b'))
+# modulus dan floor division
+b %= 3
+print("nilai b %= 3, nilai b menjadi",b)
 
-# shift left (<<)
-c = a << 2
-print('\n=========<<=========')
-print('nilai :',a,' , binary :',format(a,'08b'))
-print('----------------------------- (<<)')
-print('nilai :',c,' , binary :',format(c,'08b'))
+b = 10
+print("\nnilai b =",b)
 
+b //= 3
+print("nilai b //= 3, nilai b menjadi",b)
+
+# pangkat atau eksponen
+a = 5
+print("\nnilai a =",a)
+a **= 3
+print("nilai a **= 3, nilai a menjadi",a)
+
+# operasi bitwise
+# OR
+c = True
+print("\nnilai c =",c)
+c |= False
+print("nilai c |= False, nilai c menjadi",c)
+c = False
+print("nilai c =",c)
+c |= False
+print("nilai c |= False, nilai c menjadi",c)
+
+# AND
+c = True
+print("\nnilai c =",c)
+c &= False
+print("nilai c &= False, nilai c menjadi",c)
+c = True
+print("nilai c =",c)
+c &= True
+print("nilai c &= True, nilai c menjadi",c)
+
+# XOR
+c = True
+print("\nnilai c =",c)
+c ^= False
+print("nilai c ^= False, nilai c menjadi",c)
+c = True
+print("nilai c =",c)
+c ^= True
+print("nilai c ^= True, nilai c menjadi",c)
+
+# geser geser
+d = 0b0100
+print("\nnilai d =",format(d,'04b'))
+d >>= 2
+print("nilai d >>= 2, nilai d menjadi",format(d,'04b'))
+d <<= 1
+print("nilai d <<= 1, nilai d menjadi",format(d,'04b'))
