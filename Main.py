6319247433c79086844ae77c7570e0c50a87c5be
nasstako <80150148+nasstako@users.noreply.github.com>
2021-03-06 04:31:58 -0500

    tutorial04

--- Main.py
+++ Main.py
@@ -1,19 +1,25 @@
-import time
-start_time = time.time()
+# Variabel adalah tempat menyimpan data
 
-print("Hello")
-print("World")
-print("Hello World")
+# menaru / assignment nilai
+a = 10
+x = 5
+panjang = 1000
 
-print("Halo Chantiiek")
-# ini adalah comment
-a = 10 # ini adalah comment juga
-"""ada apa dengan
-ucup dan otong si ganteng
-dalam comment multiline"""
-print(a)
-print(time.time() - start_time, "detik")
-# kita bisa mengcompile python ke
-# yang namanya bytecode
-# cara mengcompile, buka terminal dan tuliskan
-# python -m py_compile Main.py
+# pemanggilan pertama
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+
+# penamaan
+nilai_y = 15 # dengan menggunakan underscore
+juta10 = 10000000 # ini boleh
+nilaiZ = 17.5 # ini boleh
+
+# pemanggilan kedua
+print("Nilai a = ", a)
+a = 7
+print("Nilai a = ", a)
+
+# assignment indirect
+b = a
+print("Nilai b = ", b)
