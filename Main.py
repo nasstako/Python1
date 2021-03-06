313c3d844d696c116c892677b8d86bed81e8d007
nasstako <80150148+nasstako@users.noreply.github.com>
2021-03-06 04:39:29 -0500

    tutorial10

--- Main.py
+++ Main.py
@@ -1,20 +1,89 @@
-# latihan konversi satuan temperature
+# operasi komparasi
 
-# program konversi celcius ke satuan lain
+# setiap hasil dari operasi komparasi adalah boolean
 
-print("\nPROGRAM KONVERSI TEMPERATUR\n")
+# >,<,>=,<=,==,!=,is,is not
 
-celcius = float(input('Masukan suhu dalam celcius : '))
-print("suhu adalah",celcius, "Celcius")
+a = 4
+b = 2
 
-# reamur
-reamur = (4/5) * celcius
-print("suhu dalam reamur adalah ",reamur, "Reamur")
+# lebih besar dari >
+print('========== lebih besar dari (>)')
+hasil = a > 3
+print(a,'>',3,'=',hasil)
+hasil = b > 3
+print(b,'>',3,'=',hasil)
+hasil = b > 2
+print(b,'>',2,'=',hasil)
 
-# fahrenheit
-fahrenheit = ((9/5) * celcius) + 32
-print("suhu dalam fahrenheit adalah ",fahrenheit, "Fahrenheit")
+# kurang dari <
+print('========== kurang dari (<)')
+hasil = a < 3
+print(a,'<',3,'=',hasil)
+hasil = b < 3
+print(b,'<',3,'=',hasil)
+hasil = b < 2
+print(b,'<',2,'=',hasil)
 
-# Kelvin
-kelvin = celcius + 273
-print("suhu dalam kelvin adalah ",kelvin, "Kelvin")
+# lebih dari sama dengan >=
+print('========== lebih dari sama dengan(>=)')
+hasil = a >= 3
+print(a,'>=',3,'=',hasil)
+hasil = b >= 3
+print(b,'>=',3,'=',hasil)
+hasil = b >= 2
+print(b,'>=',2,'=',hasil)
+
+# kurang dari sama dengan <=
+print('======== kurang dari sama dengan(<=)')
+hasil = a <= 3
+print(a,'<=',3,'=',hasil)
+hasil = b <= 3
+print(b,'<=',3,'=',hasil)
+hasil = b <= 2
+print(b,'<=',2,'=',hasil)
+
+# sama dengan (==)
+print('======== sama dengan(==)')
+hasil = a == 4
+print(a,'==',4,'=',hasil)
+hasil = b == 4
+print(b,'==',4,'=',hasil)
+
+# tidak sama dengan (!=)
+print('======== sama dengan(!=)')
+hasil = a != 4
+print(a,'!=',4,'=',hasil)
+hasil = b != 4
+print(b,'!=',4,'=',hasil)
+
+# 'is' sebagai komparasi object identity
+print('======== object identity(is)')
+x = 5 # ini adalah assignment membuat object
+y = 5
+print('nilai x =',x,',id = ',hex(id(x)))
+print('nilai y =',y,',id = ',hex(id(y)))
+hasil = x is y
+print('x is y =',hasil)
+
+x = 5 # ini adalah assignment membuat object
+y = 6
+print('nilai x =',x,',id = ',hex(id(x)))
+print('nilai y =',y,',id = ',hex(id(y)))
+hasil = x is y
+print('x is y =',hasil)
+
+print('======== object identity(is not)')
+x = 5 # ini adalah assignment membuat object
+y = 5
+print('nilai x =',x,',id = ',hex(id(x)))
+print('nilai y =',y,',id = ',hex(id(y)))
+hasil = x is not y
+print('x is y =',hasil)
+
+x = 5 # ini adalah assignment membuat object
+y = 6
+print('nilai x =',x,',id = ',hex(id(x)))
+print('nilai y =',y,',id = ',hex(id(y)))
+hasil = x is not y
+print('x is y =',hasil)
